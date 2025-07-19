d8b4fe5441f97c8550c20c256ea99a8f2b98da38
neagm <gageh34913@luxpolar.com>
2025-07-19 03:08:09 +0300

    welcome too

---
+++ js.js
@@ -0,0 +1,81 @@
+window.onload=function(){
+    document.write('welcome to my world')
+    document.write('welcome')
+    var heed='gfhfghf';
+    var number=20;
+}
+console.log(40)
+console.log(40)
+console.log('hisham')
+console.error('this is a mistake')
+window.print('nice man')
+window.alert('nice')
+window.document.write('hisham')
+let reed='fghfgh'
+const geed='dfgdfg'
+name.slice(0,7)
+name.split(0,4)
+name.substr()
+name.substring(0,4)
+name.split()
+Array=[4545454,44,545,5454]
+Array.splice(0,4,'fghfgh')
+Array.includes('fhfg',0)
+Array.startswith('dfgdf',0)
+Array.endeswith(0,4)
+Array.concat(Array)
+Array.join()
+Math.abs(-4556)
+Math.round()
+Math.ceil()
+Math.floor()
+Math.sqrt()
+Math.pow()
+log(typeof 'fghgfh')
+scrollTo()
+reverce()
+if(name==45){
+    console.log()
+}
+else if(name==40){
+    console.log(40)
+}
+else{
+    crossOriginIsolated.log(40)
+}
+switch(name==40){
+    case 'leed': console.log(40);
+    break;
+    case 'leed':console.log(40);
+    default:console.log(40)
+}
+name==410?
+console.log(40)
+:name==50?
+console.log(40)
+:log(40);
+for(let geed=50;geed==40;geed++){
+    console.log(40)
+}
+while(name==50){
+    console.log(40)
+}
+do{
+    console.log(40)
+}while(name==40)
+function name(){
+    return 0
+}
+name()
+(function(){
+    console.log()
+})();
+let reed4=foo=>40
+let rood={
+    beed:'grr',
+    f:function(){
+        console.log(40)
+    }
+}
+oncontextmenu()
+
